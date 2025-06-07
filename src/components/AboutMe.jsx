c9f4d23d45f10abdd80013d7489f7e533f68d599
parmeshkumarT <tparmeshkumar18506@gmail.com>
2025-06-07 19:54:25 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ export const AboutMe = () => {
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                     <div className="space-y-6 ">
-                        <h3 className="text-2xl font-semibold">passionate web developer and keen interest on Database Management Systems theorotical concepts</h3>
+                        <h3 className="text-2xl font-semibold">passionate web developer and keen interest on Database Management Systems concepts</h3>
                         <p className="text-muted-foreground">
                             i have a passionate interest in creating front-end applications
                             and single page web applications using react and also currently
