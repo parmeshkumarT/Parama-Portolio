0dbd350e2dcc5ee7ab8d94810e81a74bcba2578c
parmeshkumarT <tparmeshkumar18506@gmail.com>
2025-07-24 18:21:41 +0530

    Update ProjectSection.jsx

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -30,7 +30,7 @@ export const ProjectSection = () => {
         done by group of three including me, the other two projects were built completely by me.
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
         {projects.map((project, key) => (
           <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
             <div className="h-48 overflow-hidden ">
