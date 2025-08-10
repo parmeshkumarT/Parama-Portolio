923a4c0a1c2a37efd74f7ec77daca487b0ad2fa1
parmeshkumarT <tparmeshkumar18506@gmail.com>
2025-08-10 19:39:09 +0530

    Update SkillsSection.jsx

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -56,10 +56,6 @@ export const SkillsSection = () => {
                         <div className="text-left mb-4">
                             <h3 className="font-semibold text-lg">{skill.name}</h3>
                         </div>
-                        <div className="w-full bg-secondary/0 h-2 rounded-full overflow-hidden">
-                            <div className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]"
-                                style={{ width: skill.level + "%" }} />
-                        </div>
                         <div className="text-right mt-1">
                             <span className="text-sm text-muted-foreground">{skill.level + "%"}</span>
                         </div>
